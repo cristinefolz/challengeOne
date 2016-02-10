7c58d7276851d24d3b567488c7c23683243cc8ef
LuckXP <js.sindler@gmail.com>
2016-02-10 15:51:24 -0700

    comented out check box functions

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-function sayitworked(event){
+function toToDo(event){
+	event.preventDefault();
 	var $element = $(event.target);
 	var $form = $element.closest('form');
 	var $inputs = $form.find('input')
-	var value1 = $inputs.first().val();
-	var value2 = $inputs.eq(1).val();
-	
+	var taskName = $inputs.first().val();
+	var dueDate = $inputs.eq(1).val();
+	var $list = $('#toDoTable');
+	$list.append('<tr>\
+					<td>' + taskName + '\
+					</td>\
+					<td>' + dueDate + '\
+					</td>\
+					<td>\
+						<div class="checkbox">\
+							<label>\
+								 <input class= "unique-check" type="checkbox"> \
+							</label>\
+						</div>\
+					</td>\
+				</tr>');
+
 	console.log("hey, it worked", value1, value2);
 	
 	return false;
 
 }
 
-$('#submit-button').on('click',sayitworked);
+$('#submit-button').on('click',toToDo);
+
+
+
+// function youdidit(){
+// 	window.alert("Good work! You finished");
+// }
+
+// $('.unique-check').on($('.unique-check').checked = true), youdidit);
 
 
 
@@ -26,8 +49,25 @@ $('#submit-button').on('click',sayitworked);
 
 
 
-function youdidit(){
-	window.alert("Good work! You finished");
-}
 
-$('unique-check').on('click', youdidit);
+
+
+// function youdidit(){
+	
+// 	if($('.unique-check').checked = true)	{
+// 		window.alert("Good work! You finished");
+// 	} else if ($('.unique-check').checked = false){
+// 		window.alert("Well get to work!");
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+// $('.unique-check').on('click', youdidit);
+
+
+
+
+
+
+
