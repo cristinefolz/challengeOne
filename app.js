a04a9212f12b2b42b65991cb680def888dc6602f
LuckXP <js.sindler@gmail.com>
2016-02-11 13:57:49 -0700

    jqury dummy data injecter

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-function toToDo(event){
-	event.preventDefault();
-	var $element = $(event.target);
-	var $form = $element.closest('form');
-	var $inputs = $form.find('input')
-	var taskName = $("#taskName").val();
-	var dueDate = $("#dueDate").val();
-	var $list = $('#toDoTable');
+var taskOne = { name: "do some stuff", date: "02/16/2016" };
+var taskTwo = { name: "do some more stuff", date: "02/18/2016" };
+var $list = $('#toDoTable');	
+
+var toDos = [taskOne, taskTwo];
+
+for(var i = 0; i < toDos.length; i++){
 	$list.append('<tr>\
-					<td>' + taskName + '\
+					<td>' + toDos[i].name + '\
 					</td>\
-					<td>' + dueDate + '\
+					<td>' + toDos[i].date + '\
 					</td>\
 					<td>\
 						<div class="checkbox">\
@@ -19,9 +18,36 @@ function toToDo(event){
 						</div>\
 					</td>\
 				</tr>');
+}
 
-	console.log("hey, it worked", value1, value2);
+
+function toToDo(event){
+	event.preventDefault();
+	var $element = $(event.target);
+	var $form = $element.closest('form');
+	var $inputs = $form.find('input')
+	var taskName = $("#taskName").val();
+	var dueDate = $("#dueDate").val();
 	
+
+	if(taskName && dueDate) {
+
+		$list.append('<tr>\
+						<td>' + taskName + '\
+						</td>\
+						<td>' + dueDate + '\
+						</td>\
+						<td>\
+							<div class="checkbox">\
+								<label>\
+									 <input class= "unique-check" type="checkbox"> \
+								</label>\
+							</div>\
+						</td>\
+					</tr>');
+
+		console.log("hey, it worked", taskName, dueDate);
+	}	
 	return false;
 
 }
